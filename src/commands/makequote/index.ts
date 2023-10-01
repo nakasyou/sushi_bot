77a52991660215cfb4e527bc770a23889a2df6ec
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 20:04:35 +0900

    Update index.ts

--- src/commands/makequote/index.ts
+++ src/commands/makequote/index.ts
@@ -4,7 +4,12 @@ import { createCanvas } from '@napi-rs/canvas'
 const makequote = (async (opts) => {
   const canvas = createCanvas(1200, 600)
   const ctx = canvas.getContext("2d")
-  
+
+  if (opts.replyData) {
+    opts.reply(opts.replyData.content.body)
+  } else {
+    opts.reply('MakeItAQuote: リプライで使ってね')
+  }
   opts.reply(opts.message.replace("?echo", ""))
 }) satisfies Command
 
