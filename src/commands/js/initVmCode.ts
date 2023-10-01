37f1ae75c309aa1e3ff5da1d46b0d481eecef10f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 16:19:52 +0900

    stringify

--- src/commands/js/initVmCode.ts
+++ src/commands/js/initVmCode.ts
@@ -1,30 +1,6 @@
 const initVmCode = `
-const _stringify = (data, objects) => {
-  if (!objects) {
-    objects = {};
-  }
-  for (const [key, value] of Object.entries(data)) {
-    if (typeof value === "function" || value instanceof Function) {
-      data[key] = value.toString();
-    }
-    for (const [objectKey, objectValue] of Object.entries(objects)){
-      if (Object.is(objectValue, value)) {
-        data[key] = \`[circular \${objectKey}]\`;
-        delete objects[objectKey];
-        break;
-      }
-    }
-    try {
-      JSON.stringify(value);
-    } catch (_error) {
-      objects[key] = value;
-      _stringify(value, objects);
-    }
-  }
-  return JSON.stringify(data, null, 2)
-};
-
-const end = (data) => postMessage(_stringify(data));
+import { stringify } from "javascript-stringify";
+const end = (data) => postMessage(stringify(data, null, 2));
 `.replaceAll("\n", "")
 
 export default initVmCode
