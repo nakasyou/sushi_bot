fdfc841f74aff0c37200306fca4a0e804c6d296d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 20:28:39 +0900

    Update index.ts

--- src/commands/makequote/index.ts
+++ src/commands/makequote/index.ts
@@ -1,16 +1,97 @@
 import type { Command } from '../../main.ts'
-import { createCanvas } from '@napi-rs/canvas'
+import { createCanvas, loadImage } from '@napi-rs/canvas'
 
 const makequote = (async (opts) => {
+  const { client } = opts
+  if (!opts.replyData) {
+    opts.reply(opts.replyData.content.body)
+  }
+  
   const canvas = createCanvas(1200, 600)
   const ctx = canvas.getContext("2d")
 
-  if (opts.replyData) {
-    opts.reply(opts.replyData.content.body)
-  } else {
-    opts.reply('MakeItAQuote: リプライで使ってね')
+  const profileInfo = await client.getProfileInfo(opts.replyData.target)
+
+  const avatarUrl = client.mxcUrlToHttp(profile.avatar_url, 600, 600) // アバターのHTTP URL
+  const avatar = loadImage(avatarUrl)
+
+  // 背景の描画
+  ctx.fillStyle = "#000"
+  ctx.fillRect(0, 0, 1200, 600)
+
+  // アバターの描画
+  ctx.drawImage(image, 0, 0, 600, 600)
+
+  // アバターをそれっぽく
+  const startR = 500
+  let r = startR
+  ctx.lineWidth = 5
+  while (true) {
+    const wariai = (r - startR) / (1000 - startR)
+    ctx.beginPath()
+    ctx.arc(-400, 400, r, 0, Math.PI * 2, true)
+    ctx.strokeStyle = `rgba(0, 0, 0, ${wariai < 0 ? 0 : wariai})`
+    ctx.stroke()        
+    r += 5
+    if (r > 1500) {
+      break
+    }
   }
-  opts.reply(opts.message.replace("?echo", ""))
+
+  // テキストの描画
+  ctx.fillStyle = '#fff'
+  ctx.font = "50px sans-serif";
+  let x = 0
+  let y = 0
+  ctx.textAlign = "center"
+  const charDatas = []
+  for (const char of reply.msg) {
+    const charSize = ctx.measureText(char)
+    if (x + charSize.width > 550) {
+      x = 0
+      y += 60
+    }
+    x += charSize.width
+    charDatas.push({
+      x,y,
+      width: charSize.width,
+      char,
+      charSize
+    })
+  }
+  const xBias = (600 - x) / 2 + 450
+  const yBias = (400 - y) / 2 + 20
+  for (const charData of charDatas) {
+    ctx.fillText(charData.char, charData.x + xBias - charData.charSize.actualBoundingBoxLeft, charData.y + yBias)
+  }
+
+  // 名前
+  ctx.textAlign = "center"
+  ctx.font = "italic 40px sans-serif";
+  ctx.fillText('-' + displayname, 900, 500);
+  // ユーザーID
+  ctx.font = "30px sans-serif";
+  ctx.fillStyle = '#bbb'
+  ctx.fillText(reply.target, 900, 550);
+  const url = canvas.toDataURL();
+
+  const pngData: Uint8Array = await canvas.encode('png') // Uint8Array
+  
+  const uploaded = client.uploadContent(pngData, {
+    type: 'image/png',
+    name: 'image.png'
+  })
+
+  opts.reply(uploaded.content_uri)
+  opts.reply('Image', {
+    "info": {
+      "h": 600,
+      "mimetype": "image/png",
+      "w": 1200
+    },
+    "msgtype": "m.image",
+    "url": uploaded.content_uri
+  })
 }) satisfies Command
 
 export default makequote
