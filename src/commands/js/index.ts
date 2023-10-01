30be740e0734ee2795bc37463a2c676c51547922
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 17:25:43 +0900

    Update index.ts

--- src/commands/js/index.ts
+++ src/commands/js/index.ts
@@ -1,6 +1,8 @@
 import type { Command } from '../../main.ts'
 import initVmCode from './initVmCode.ts'
 
+import $ from 'dax'
+
 const js = (async (opts) => {
   /**
    * メッセージの行
@@ -15,34 +17,31 @@ const js = (async (opts) => {
   /**
    * 実行するコード
    */
-  const code = initVmCode + "\n" + bodyCode
+  const code = (initVmCode + "\n" + bodyCode).replaceAll("'", "\\'")
 
-  const evalCommand = new Deno.Command('deno', {
-    args: ['eval', code],
-    stdout: 'piped',
-    stderr: 'piped',
-  })
-  const process = evalCommand.spawn()
+  const result = $`echo ${code} | deno run -`.stdout("piped")
+    .stderr("piped");
   
-  const status = await process.status
-  
-  const stdoutText = await new Response(process.stdout).text()
-  const stderrText = await new Response(process.stderr).text()
-  if (status.success) {
-    opts.reply(`コードの実行に成功しました!
-
-Result:
-\`\`\`js
-${stdoutText}
-\`\`\``)
+  const stdoutText = result.stdout //await new Response(process.stdout).text()
+  const stderrText = result.stderr //await new Response(process.stderr).text()
+  if (result.code === 0) {
+    opts.reply(`
+      コードの実行に成功しました!
+      
+      Result:
+      \`\`\`js
+      ${stdoutText}
+      \`\`\`
+    `.slice(1, -1).split('\n').map(line => line.slice(6)).join('\n'))
   } else {
     // エラー
-    opts.reply(`コードの実行に失敗しました...
-    
-Error:
-\`\`\`
-${stderrText}
-\`\`\``)
+    opts.reply(`
+      コードの実行に失敗しました...
+      Error:
+      \`\`\`
+      ${stderrText}
+      \`\`\`
+    `.slice(1, -1).split('\n').map(line => line.slice(6)).join('\n'))
   }
 }) satisfies Command
 
