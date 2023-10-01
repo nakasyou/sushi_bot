1591db0658fb5b4a7b4a20d65900f0c2c75a6d40
nakasyou <syoutarou1103@outlook.jp>
2023-10-01 19:10:17 +0900

    canvas

--- src/commands/makequote/index.ts
+++ src/commands/makequote/index.ts
@@ -1,8 +1,9 @@
 import type { Command } from '../../main.ts'
 import { createCanvas } from 'canvas'
 
-const echo = (async (opts) => {
+const makequote = (async (opts) => {
+  const canvas = createCanvas(1200, 600)
   opts.reply(opts.message.replace("?echo", ""))
 }) satisfies Command
 
-export default echo
+export default makequote
