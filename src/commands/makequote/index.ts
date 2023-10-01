952762c67b461634e00ab61c65be7effefe44c4a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 19:56:44 +0900

    Update index.ts

--- src/commands/makequote/index.ts
+++ src/commands/makequote/index.ts
@@ -1,8 +1,10 @@
 import type { Command } from '../../main.ts'
-import { createCanvas } from 'canvas'
+import { createCanvas } from '@napi-rs/canvas'
 
 const makequote = (async (opts) => {
   const canvas = createCanvas(1200, 600)
+  const ctx = canvas.getContext("2d")
+  
   opts.reply(opts.message.replace("?echo", ""))
 }) satisfies Command
 
