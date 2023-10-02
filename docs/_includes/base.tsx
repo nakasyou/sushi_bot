1cdfe3f9c83b576aa07c575234d5bad114cbf5f7
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 20:54:43 +0900

    Create base.tsx

---
+++ docs/_includes/base.tsx
@@ -0,0 +1,11 @@
+export default (props) => <html lang='ja'>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <meta name="generator" content="Lume" />
+  </head>
+  <body>
+    { props.children }
+  </body>
+</html>
