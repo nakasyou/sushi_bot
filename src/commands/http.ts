5f7e56182cace90c6334867acd1b6c8207382fb0
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 11:42:08 +0900

    Create http.ts

---
+++ src/commands/http.ts
@@ -0,0 +1,15 @@
+import type { Command } from '../main.ts'
+
+const http = (async (opts) => {
+  const textUrl = opts.message.replace('?netinfo ', '')
+  let url: URL
+  try {
+    url = new URL(textUrl)
+  } catch (_error) {
+    opts.reply("正しいURLを入力してください")
+  }
+  const response = await fetch(url)
+  opts.reply(reponse.status)
+}) satisfies Command
+
+export default http
