7a996af43bf1c7fc3d7a8af75779c6665fc1f7c8
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 17:51:56 +0900

    Update http.ts

--- src/commands/http.ts
+++ src/commands/http.ts
@@ -7,8 +7,12 @@ const http = (async (opts) => {
   let url: URL
   try {
     url = new URL(textUrl)
+    if (!['http', 'https'].includes(url.protocol)) {
+      throw new Error()
+    }
   } catch (_error) {
     opts.reply("正しいURLを入力してください")
+    return
   }
   const response = await fetch(url)
   opts.reply(reponse.status)
