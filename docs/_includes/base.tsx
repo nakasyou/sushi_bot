bd9d7cce48a9c8003f7957bd71ffab6d8cbe8508
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 20:56:14 +0900

    Update base.tsx

--- docs/_includes/base.tsx
+++ docs/_includes/base.tsx
@@ -1,9 +1,18 @@
-export default (props) => <html lang='ja'>
+export default (props: {
+  children: JSX.Element
+  title: string
+  styles?: string[]
+}) => <html lang='ja'>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <meta name="generator" content="Lume" />
+    <title>{ props.title }</title>
+
+    {
+      (props.styles || []).map(style => <style>{ style }</style>)
+    }
   </head>
   <body>
     { props.children }
