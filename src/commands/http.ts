76cafbd448d565de8b3de0caaa21c6a6124d1352
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 17:58:07 +0900

    Update http.ts

--- src/commands/http.ts
+++ src/commands/http.ts
@@ -12,10 +12,13 @@ const http = (async (opts) => {
     await opts.reply("正しいURLを入力してください")
     return
   }
+  
+  let response: Response
   try {
-    const response = await fetch(url)
+    response = await fetch(url)
   } catch (_error) {
     await opts.reply('httpリクエストの送信中にエラーが発生しました。正しく名前解決ができなかったり、プロトコルが間違っている可能性があります。')
+    return
   }
 
   await opts.reply(reponse.status)
