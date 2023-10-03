9d1777a935956928d856f68e314bf82041c0d7bd
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-03 10:32:57 +0900

    Update and rename index.mdx to index.tsx

---
+++ docs/index.tsx
@@ -0,0 +1,11 @@
+import Base from './_includes/base.tsx'
+
+export default () => <Base>
+  <div className="text-center">
+    <img src="https://emoji2svg.deno.dev/api/🍣" alt="🍣" width="128" height="128"/>
+    <div className="text-center font-bold">
+      お寿司Bot🍣
+    </div>
+  </div>
+  
+</Base>
